e6992b0b1ffa50b45db91a98ec7d621af6fd2b76
Pratima9145 <pratimabhat98@gmail.com>
2019-12-07 15:41:26 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,34 @@
-// Include form validation functions here
+function validation() {
+	var a=document.forms["movieform"]["txtTitle"].value;
+	if(a=="") {
+		alert("Title is required");
+		return false;
+	}
+	if((a.length <2) || (a.length > 100)) {
+		alert("Title should have 2 to 100 characters");
+		document.forms["movieform"]["txtTitle"].select();
+		return false;
+	}
+	var b=document.forms["movieform"]["txtGrosss"].value;
+	if(b=="") {
+		alert("Gross is required");
+		document.forms["movieform"]["txtGrosss"].focus();
+		return false;
+	}
+	if(/[^0-9]/.test(b)) {
+		alert("Gross has to be a number");
+		return false;
+	}
+	var c=document.forms["movieform"]["txtLaunch"].value;
+	if(c=="") {
+		alert("Date of launch is required");
+		document.forms["movieform"]["txtLaunch"].focus();
+		return false;
+	}
+	var d=document.forms["movieform"]["genres"].value;
+		if(d=="") {
+			alert("Select one genre");
+			return false;
+		}
+	alert("Details submitted successfully");
+}
