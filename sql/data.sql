17bd5fa40f70b1d32b8c70e73ce4fdf9934a10fe
Pratima9145 <pratimabhat98@gmail.com>
2020-01-10 14:11:55 +0530

    sql module

--- sql/data.sql
+++ sql/data.sql
@@ -1,1 +1,103 @@
 -- Include table data insertion, updation, deletion and select scripts
+
+-- -------------------------------------------------------------------
+-- Adding Movie Items in MovieCruiser Table
+-- -------------------------------------------------------------------
+INSERT INTO movie_item
+VALUES 
+(1, 'Avatar', 2787965087, 'Yes', '2017-03-15','Science Fiction', 'Yes'),
+(2, 'The Avengers', 1518812988, 'Yes', '2017-12-23', 'Superhero', 'No'),
+(3, 'Titanic', 2187463944, 'Yes','2017-08-21', 'Romance', 'No'),
+(4, 'Jurassic World', 1671713208, 'No','2017-07-02','Science Fiction', 'Yes'),
+(5, 'Avengers: End Game', 2750760348, 'Yes', '2022-11-02','Superhero', 'Yes');
+
+-- ---------------------------------------------------------
+-- Displaying Movie Items --
+-- -----------------------------------------------------------------
+select * from movie_item;
+
+-- -------------------------------------------------------------------
+-- Edit Movie Items in Movie Cruiser Table
+-- -------------------------------------------------------------------
+update movie_item 
+set movie_title='The Gentlemen',movie_gross=2589631478,movie_genre='Action Crime'
+where movie_id=1;
+
+-- -------------------------------------------------------------------
+-- Displaying Updated movies in Movie Cruiser Table
+-- -------------------------------------------------------------------
+select * from movie_item;
+
+-- -------------------------------------------------------------------
+-- Displaying Customer Movie List in MovieItem Table
+-- -------------------------------------------------------------------
+select movie_title,movie_gross,movie_genre,movie_has_teaser
+from movie_item
+where movie_active='Yes' 
+and movie_date_of_launch <= current_date();
+
+-- -------------------------------------------------------------------
+--  setting User Name and Id
+-- -------------------------------------------------------------------
+INSERT INTO user 
+VALUES
+(1,'Vinayak'),
+(2,'Asha');
+
+-- ---------------------------------------------------------
+-- Displaying User Selected Items --
+-- -----------------------------------------------------------------
+select * from user;
+
+-- -------------------------------------------------------------------
+--  Add to Favorites Table
+-- -------------------------------------------------------------------
+insert into favorites(ft_us_id,ft_pr_id) 
+values(2,1),(2,2),(2,5),(2,1),(1,1),(1,4);
+
+-- -------------------------------------------------------------------
+--  Show Favorites Movie Items
+-- -------------------------------------------------------------------
+select * from favorites;
+
+-- -------------------------------------------------------------------
+--  Show Favorites Movie Items
+-- -------------------------------------------------------------------
+SELECT movie_title,movie_gross,movie_genre
+FROM movie_item 
+INNER JOIN favorites
+ON ft_pr_id=movie_id
+WHERE ft_us_id=2;
+
+-- -------------------------------------------------------------------
+--  Total Price of Favorites Movie Items
+-- -------------------------------------------------------------------
+select count(movie_title) as no_of_favorites from movie_item
+inner join favorites on ft_pr_id=movie_id
+where ft_us_id=2; 
+
+-- -----------------------------------------------------
+-- Delete Favorite Items from Favorites Table
+-- -----------------------------------------------------
+delete from favorites
+where ft_us_id=2
+and ft_pr_id=1
+limit 1; 
+
+-- -------------------------------------------------------------------
+--  View Remove Favorites
+-- -------------------------------------------------------------------
+SELECT movie_title,movie_gross,movie_genre
+FROM movie_item  
+INNER JOIN favorites 
+ON ft_pr_id=movie_id
+WHERE ft_us_id=2;
+
+-- -------------------------------------------------------------------
+--  Show Total Number of Favorites Items
+-- -------------------------------------------------------------------
+select count(movie_title) as no_of_favorites from movie_item
+inner join favorites on ft_pr_id=movie_id
+where ft_us_id=2; 
+ 
+
